9c889567513aafe8185bfda2ec7f1698b2c98a1d
Jonas Weber <jonas@jole.dev>
2024-02-15 15:34:34 +0100

    Use HTTPS for cloning dotfiles

--- 20_dotfiles.sh
+++ 20_dotfiles.sh
@@ -3,6 +3,6 @@
 set -e
 
 sudo pacman --noconfirm -S git stow
-git clone git@github.com:drjole/dotfiles.git ~/.dotfiles
+git clone https://github.com/drjole/dotfiles ~/.dotfiles
 cd ~/.dotfiles
 stow .
