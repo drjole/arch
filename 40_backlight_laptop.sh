c406809c1272cc776669570e7f83f0ed15e0841c
Jonas Weber <jonas@jole.dev>
2024-02-21 12:58:44 +0100

    Remove unnecessary sudo keep-alives

--- 40_backlight_laptop.sh
+++ 40_backlight_laptop.sh
@@ -2,14 +2,6 @@
 
 set -e
 
-# Ask for the sudo password upfront and make sure it is not asked for again
-sudo -v
-while true; do
-  sudo -n true
-  sleep 60
-  kill -0 "$$" || exit
-done 2>/dev/null &
-
 sudo pacman --noconfirm -S acpilight
 sudo usermod -aG video "$USER"
 sudo cat <<EOF >/etc/udev/rules.d/90-backlight.rules
--- 99_bluetooth.sh
+++ 99_bluetooth.sh
@@ -2,13 +2,5 @@
 
 set -e
 
-# Ask for the sudo password upfront and make sure it is not asked for again
-sudo -v
-while true; do
-  sudo -n true
-  sleep 60
-  kill -0 "$$" || exit
-done 2>/dev/null &
-
 sudo pacman --noconfirm -S blueman bluez bluez-utils
 sudo systemctl enable --now bluetooth.service
