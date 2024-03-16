4cc025a0bd228f4b88e9229f41b6433cd9d15766
Jonas Weber <jonas@jole.dev>
2024-03-16 13:28:06 +0100

    Add TeX Live dependencies

--- 60_texlive.sh
+++ 60_texlive.sh
@@ -2,5 +2,12 @@
 
 set -e
 
+# Install ghostscript which is required for eps to pdf conversion
+sudo pacman --noconfirm -S ghostscript
+
+# Install TeX Live
 cd ./install-tl-20231025
 sudo perl ./install-tl --no-interaction
+
+# Install missing CPAN modules for latexindent
+yes | sudo cpan YAML::Tiny File::HomeDir
