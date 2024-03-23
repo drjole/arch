79e5baa03070ea405a823735f1e2a24e12ca40bb
Jonas Weber <jonas@jole.dev>
2024-03-23 16:03:47 +0100

    Install TeX Live via pacman

--- 60_texlive.sh
+++ 60_texlive.sh
@@ -2,12 +2,4 @@
 
 set -e
 
-# Install ghostscript which is required for eps to pdf conversion
-sudo pacman --noconfirm -S ghostscript
-
-# Install TeX Live
-cd ./install-tl-20231025
-sudo perl ./install-tl --no-interaction
-
-# Install missing CPAN modules for latexindent
-yes | sudo cpan YAML::Tiny File::HomeDir
+sudo pacman --noconfirm -S texlive biber ghostscript perl-yaml-tiny perl-file-homedir
