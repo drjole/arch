a15775e915f414fc76a8804b6dd2067851afbd43
Jonas Weber <jonas@jole.dev>
2024-10-14 12:27:13 +0200

    Don't manage global gems

--- update_packages.sh
+++ update_packages.sh
@@ -1,7 +1,11 @@
+#!/bin/bash
+
+set -e
+
 sudo pacman -Syu
 yay -Syu
 cargo install-update --all
-gem update
+
 xargs -I {} go install -v {} <packages.go.common
 local_go_packages_file="packages.go.$(hostname)"
 [ -f "$local_go_packages_file" ] && xargs -I {} go install -v {} <"$local_go_packages_file"
