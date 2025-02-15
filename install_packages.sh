a437f525435d7e1bb153a2b95e2469256c891b5e
Jonas Weber <jonas@jole.dev>
2025-02-15 10:58:19 +0100

    Format install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -16,89 +16,89 @@ set -e
 # A function for parsing a packages file.
 # Given the file name, it reads the file and returns a list of packages to install.
 parse_packages_file() {
-  local file="$1"
-  if [[ ! -f "$file" ]]; then
-    return
-  fi
-
-  local packages=()
-  while read -r line; do
-    # Skip comments and empty lines
-    if [[ "$line" =~ ^#.*$ ]] || [[ -z "$line" ]]; then
-      continue
+    local file="$1"
+    if [[ ! -f "$file" ]]; then
+        return
     fi
-    if [[ "$line" == %* ]]; then
-      group_packages=($(parse_group_line "$line"))
-      packages=(${packages[@]} ${group_packages[@]})
-    else
-      packages+=("$line")
-    fi
-  done <"$file"
 
-  echo "${packages[@]}"
+    local packages=()
+    while read -r line; do
+        # Skip comments and empty lines
+        if [[ "$line" =~ ^#.*$ ]] || [[ -z "$line" ]]; then
+            continue
+        fi
+        if [[ "$line" == %* ]]; then
+            group_packages=($(parse_group_line "$line"))
+            packages=(${packages[@]} ${group_packages[@]})
+        else
+            packages+=("$line")
+        fi
+    done <"$file"
+
+    echo "${packages[@]}"
 }
 
 # A function for parsing a '%group minus package1 package2 ...' line.
 # Given the line as the first argument, it returns a list of packages to install from the group excluding the specified packages.
 parse_group_line() {
-  local line="$1"
-  local group_name=$(echo "$line" | cut -d' ' -f1 | sed 's/^%//')
-  local group_packages=($(pacman -Sgq "$group_name"))
-  local minus_packages=$(echo "$line" | sed -n 's/^%.* minus //p')
-  for minus_package in $minus_packages; do
-    group_packages=($(echo "${group_packages[@]}" | sed "s/\b$minus_package\b//g"))
-  done
-  echo "${group_packages[@]}"
+    local line="$1"
+    local group_name=$(echo "$line" | cut -d' ' -f1 | sed 's/^%//')
+    local group_packages=($(pacman -Sgq "$group_name"))
+    local minus_packages=$(echo "$line" | sed -n 's/^%.* minus //p')
+    for minus_package in $minus_packages; do
+        group_packages=($(echo "${group_packages[@]}" | sed "s/\b$minus_package\b//g"))
+    done
+    echo "${group_packages[@]}"
 }
 
 # Install packages.
 # The package manager is passed as the first argument.
 install_packages() {
-  local package_manager="$1"
-  local packages_common=($(parse_packages_file "packages.$package_manager.common"))
-  local packages_host=($(parse_packages_file "packages.$package_manager.$(hostname)"))
-  local packages=()
-  if [[ ${#packages_common[@]} -gt 0 ]]; then
-    packages=(${packages[@]} ${packages_common[@]})
-  fi
-  if [[ ${#packages_host[@]} -gt 0 ]]; then
-    packages=(${packages[@]} ${packages_host[@]})
-  fi
+    local package_manager="$1"
+    local packages_common=($(parse_packages_file "packages.$package_manager.common"))
+    local packages_host=($(parse_packages_file "packages.$package_manager.$(hostname)"))
+    local packages=()
+    if [[ ${#packages_common[@]} -gt 0 ]]; then
+        packages=(${packages[@]} ${packages_common[@]})
+    fi
+    if [[ ${#packages_host[@]} -gt 0 ]]; then
+        packages=(${packages[@]} ${packages_host[@]})
+    fi
 
-  if [[ "$package_manager" == "pacman" ]]; then
-    sudo pacman -S --needed "${packages[@]}"
-  fi
-  if [[ "$package_manager" == "yay" ]]; then
-    yay -S --needed "${packages[@]}"
-  fi
-  if [[ "$package_manager" == "cargo" ]]; then
-    cargo install "${packages[@]}"
-  fi
-  if [[ "$package_manager" == "gem" ]]; then
-    # gem update only works with a single gem at a time so we loop here
-    for package in "${packages[@]}"; do
-      if gem list -i "$package" >/dev/null; then
-        gem update "$package"
-      else
-        gem install "$package"
-      fi
-    done
-  fi
-  if [[ "$package_manager" == "go" ]]; then
-    # go install only works with a single package at a time so we loop here
-    for package in "${packages[@]}"; do
-      go install "$package"
-    done
-  fi
-  if [[ "$package_manager" == "npm" ]]; then
-    # Install with prefix ~/.local which then must be set in the npm_config_prefix environment variable
-    npm install -g --prefix ~/.local "${packages[@]}"
-  fi
+    if [[ "$package_manager" == "pacman" ]]; then
+        sudo pacman -S --needed "${packages[@]}"
+    fi
+    if [[ "$package_manager" == "yay" ]]; then
+        yay -S --needed --devel "${packages[@]}"
+    fi
+    if [[ "$package_manager" == "cargo" ]]; then
+        cargo install "${packages[@]}"
+    fi
+    if [[ "$package_manager" == "gem" ]]; then
+        # gem update only works with a single gem at a time so we loop here
+        for package in "${packages[@]}"; do
+            if gem list -i "$package" >/dev/null; then
+                gem update "$package"
+            else
+                gem install "$package"
+            fi
+        done
+    fi
+    if [[ "$package_manager" == "go" ]]; then
+        # go install only works with a single package at a time so we loop here
+        for package in "${packages[@]}"; do
+            go install "$package"
+        done
+    fi
+    if [[ "$package_manager" == "npm" ]]; then
+        # Install with prefix ~/.local which then must be set in the npm_config_prefix environment variable
+        npm install -g --prefix ~/.local "${packages[@]}"
+    fi
 }
 
 install_packages "pacman"
 install_packages "yay"
-
+rustup update
 install_packages "cargo"
 install_packages "gem"
 install_packages "go"
