5be8c430c26ae0e41043af8403ffd1e055b3ed74
Jonas Weber <jonas@jole.dev>
2025-07-31 21:34:37 +0200

    WIP

--- new_jolepc.sh
+++ new_jolepc.sh
@@ -22,9 +22,9 @@ grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB --re
 grub-mkconfig -o /boot/grub/grub.cfg
 
 ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
-# systemctl enable systemd-timesyncd.service
-sed -i '/^#en_US.UTF-8 UTF-8/s/^#//' /etc/locale.gen
-sed -i '/^#de_DE.UTF-8 UTF-8/s/^#//' /etc/locale.gen
+
+# Enable en_US.UTF-8 locale in /etc/locale.gen
+# Enable de_DE.UTF-8 locale in /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >/etc/locale.conf
 
@@ -34,7 +34,7 @@ passwd root
 useradd -m -G wheel -s /bin/zsh jole
 passwd jole
 
-sed -i '/^# %wheel ALL=(ALL:ALL) ALL/s/^#//' /etc/sudoers
+# Uncomment the wheel `wheel ALL=(ALL:ALL) ALL` line in /etc/sudoers
 
 systemctl enable NetworkManager.service
 
@@ -50,30 +50,49 @@ reboot
 # 	Set GRUB_TIMEOUT_STYLE=hidden
 # 	Run sudo grub-mkconfig -o /boot/grub/grub.cfg
 
-sudo sed -i '/^#ParallelDownloads/s/^#//' /etc/pacman.conf
-sudo sed -i "/^MAKEFLAGS=/cMAKEFLAGS=\"-j$(nproc)\"" /etc/makepkg.conf
-sudo sed -i '/\[multilib\]/,/Include = \/etc\/pacman.d\/mirrorlist/s/^#//' /etc/pacman.conf
+# Set `ParallelDownloads = 5` in /etc/pacman.conf
+# Set `MAKEFLAGS="-j$(nproc)" in /etc/makepkg.conf
+# Uncomment the `[multilib]` section in /etc/pacman.conf
 
 sudo pacman -S \
     mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon \
     qt5-wayland qt6-wayland \
     noto-fonts noto-fonts-emoji ttf-jetbrains-mono-nerd ttf-font-awesome otf-font-awesome \
-    ly hyprland xdg-desktop-portal-hyprland hyprpolkitagent hyprpaper wofi dunst hyprlock wl-clipboard hyprsunset \
+    hyprland uwsm libnewt xdg-desktop-portal-hyprland hyprpolkitagent hyprpaper rofi dunst hyprlock wl-clipboard hyprsunset grim slurp \
     waybar network-manager-applet \
     pipewire pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack wireplumber \
-    alacritty firefox thunar spotify-launcher discord pavucontrol gimp inkscape libreoffice-still nextcloud-client signal-desktop vlc zathura zathura-pdf-poppler keepassxc steam teamspeak3 chromium \
-    gnome-themes-extra qt5ct qt6ct kvantum breeze-icons \
+    alacritty firefox thunar spotify-launcher discord pavucontrol gimp inkscape libreoffice-still nextcloud-client signal-desktop vlc zathura zathura-pdf-poppler keepassxc steam teamspeak3 chromium gnome-keyring \
+    qt5ct qt6ct kvantum breeze-icons \
     zsh-autosuggestions zsh-completions zsh-syntax-highlighting \
-    fzf starship eza bat htop tmux ddcutil man-db ripgrep stow fd lazygit \
+    fzf starship eza bat htop tmux ddcutil man-db ripgrep stow fd lazygit jq \
     blueman bluez bluez-utils \
     pacman-contrib \
-    docker mise mkcert pre-commit
+    docker docker-compose docker-buildx mise pre-commit
+
+systemctl --user enable --now hyprpaper.service
+systemctl --user enable --now hyprpolkitagent.service
+systemctl --user enable --now waybar.service
+
+# Configure GNOME Keyring to automatically unlock on login
+# NOTE: Set the same password for the keyring as for the login
+#
+# In /etc/pam.d/login:
+# Add `auth       optional     pam_gnome_keyring.so` to the end of the auth section
+# Add `session    optional     pam_gnome_keyring.so auto_start` to the end of the session section
+
+# Rust
+sudo pacman -S rustup
+rustup default stable
+
+# reditus
+sudo pacman -S mkcert postgresql
 
 git clone https://aur.archlinux.org/yay.git /tmp/yay
 pushd /tmp/yay
 makepkg -si
 popd
 
+sudo pacman -S papirus-icon-theme
 yay -S catppuccin-cursors-mocha catppuccin-gtk-theme-mocha
 
 git clone --recurse-submodules https://github.com/drjole/dotfiles ~/.dotfiles
@@ -82,16 +101,6 @@ git remote set-url origin git@github.com:drjole/dotfiles.git
 find . -mindepth 1 -maxdepth 1 -type d -not -name .git -printf "%f\n" | xargs -I {} mkdir -p "$HOME"/{}
 stow .
 popd
-
-sudo systemctl --enable ly.service
-# Change in /etc/ly/config.ini
-#   tty = 1
-#   brightness_up_key = null
-#   brightness_down_key = null
-#   hide_key_hints = true
-#   sleep_key = F10
-#   restart_key = F11
-#   shutdown_key = F12
 
 sudo systemctl enable --now paccache.timer
 
@@ -108,5 +117,12 @@ mise use -g usage node ruby
 sudo systemctl enable --now docker.service
 sudo usermod -a -G docker jole
 
-ssh-keygen -t ed25519
-echo "SSH key generated. Add it at least to GitHub"
+# Make sensors work
+echo nct6775 | sudo tee /etc/modules-load.d/nct6775.conf
+echo "options nct6775 force_id=0xd802" | sudo tee /etc/modprobe.d/nct6775.conf
+
+# Firefox
+# Set `intl.regional_prefs.use_os_locales` to true to use german date formats in firefox while keeping language at english
+
+sudo pacman -S lua-language-server
+npm install -g @tailwindcss/language-server
