e49930421d57214601b2bae49110b747e92a6ee4
Jonas Weber <jonas@jole.dev>
2025-08-16 21:24:08 +0200

    more --needed

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ else
 fi
 
 # Install all the packages
-sudo pacman -Syu --noconfirm \
+sudo pacman -Syu --noconfirm --needed \
     mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon \
     pipewire pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack wireplumber \
     hyprland uwsm libnewt xdg-desktop-portal-hyprland hyprpolkitagent hyprpaper waybar rofi dunst hyprlock wl-clipboard hyprsunset grim slurp qt5-wayland qt6-wayland \
