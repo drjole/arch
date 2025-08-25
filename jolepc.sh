02b1d73fd089d8b3f179d81d1ee709823ae0dab9
Jonas Weber <jonas@jole.dev>
2025-08-25 19:03:08 +0200

    Remove ic2 settings from jolepc.sh

--- jolepc.sh
+++ jolepc.sh
@@ -2,14 +2,6 @@
 
 set -e
 
-# Brightness control
-sudo usermod -aG i2c "$USER_NAME"
-if [ ! -e "/etc/modules-load.d/ic2-dev.conf" ]; then
-    cat <<EOF | sudo tee /etc/modules-load.d/i2c-dev.conf >/dev/null
-i2c-dev
-EOF
-fi
-
 # Sensors
 if [ ! -e "/etc/modules-load.d/nct6775.conf" ]; then
     echo nct6775 | sudo tee /etc/modules-load.d/nct6775.conf
